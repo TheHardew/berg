e8be6f08e7abd213ce99251fd469ae3fa77213fe
TheHardew <7374061+TheHardew@users.noreply.github.com>
2023-06-15 21:32:33 +0200

    get file names from the command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use berg::{BionicTransformer, EpubDocument};
 use std::{error::Error, fs, io::Write, time::Instant};
+use std::env;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let start = Instant::now();
-    let mut out = fs::File::create("rust_.epub").unwrap();
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        eprintln!("Usage: {} source.epub dest.epub", args[0]);
+        return Ok(());
+    }
 
-    let mut epub = EpubDocument::open("rust.epub")?;
+    let start = Instant::now();
+    let mut epub = EpubDocument::open(&args[1])?;
+    let mut out = fs::File::create(&args[2]).unwrap();
+
 
     epub.transform(BionicTransformer::new(), &mut out)?;
 
